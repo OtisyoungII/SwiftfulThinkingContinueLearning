53daece894f0c00c56b5121587f0ffc1f62a3c0a
Otis YII <oyoung21@gmail.com>
2025-02-19 01:12:26 -0500

    Initial Commit

---
+++ SwiftfulThinkingContinueLearning/SwiftfulThinkingContinueLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContinueLearningApp.swift
+//  SwiftfulThinkingContinueLearning
+//
+//  Created by Otis Young on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContinueLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
