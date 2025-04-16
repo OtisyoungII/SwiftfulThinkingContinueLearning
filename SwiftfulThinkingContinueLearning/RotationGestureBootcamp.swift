468e1eb1f0cc394618f6a29c679f871a81e7199b
Otis YII <oyoung21@gmail.com>
2025-04-15 21:32:29 -0400

    just some learning need to update pc in the middle of a lesson.

---
+++ SwiftfulThinkingContinueLearning/RotationGestureBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  RotationGestureBootcamp.swift
+//  SwiftfulThinkingContinueLearning
+//
+//  Created by Otis Young on 4/15/25.
+//
+
+import SwiftUI
+
+struct RotationGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotationGestureBootcamp()
+}
